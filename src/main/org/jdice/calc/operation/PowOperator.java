5da6fd0182bad8fd8912fc1bb134b05b04105b60
Davor Sauer <davor.sauer@gmail.com>
2014-04-22 23:23:37 +0200

    Update PowOpetaror logic to enable power with decimal and negative numbers

--- src/main/org/jdice/calc/operation/PowOperator.java
+++ src/main/org/jdice/calc/operation/PowOperator.java
@@ -21,6 +21,8 @@ import java.math.BigDecimal;
 import org.jdice.calc.AbstractCalculator;
 import org.jdice.calc.Num;
 import org.jdice.calc.Operator;
+import org.jdice.calc.Properties;
+import org.jdice.calc.Rounding;
 
 /**
  * 
@@ -40,10 +42,36 @@ public class PowOperator implements Operator {
     }
 
     @Override
-    public Num calc(AbstractCalculator calc, Num value1, Num value2)  {
-        BigDecimal value = value1.toBigDecimal().pow(value2.toBigDecimal().intValue());
-        Num result = new Num(value);
-        return result;
+    public Num calc(AbstractCalculator calc, Num value1, Num exponent)  {
+        int scale = Properties.getInheritedScale(calc, exponent);
+        Rounding roundingMode = Properties.getInheritedRoundingMode(calc, exponent);
+
+        return calc(calc, value1, exponent, scale, roundingMode);
+    }
+
+    public Num calc(AbstractCalculator calc, Num value1, Num exponent, Integer scale, Rounding roundingMode)  {
+        if (exponent.hasFraction()) {
+            double result = Math.pow(value1.doubleValue(), exponent.doubleValue());
+            return new Num(result);
+        } else {
+            int ex = exponent.intValue();
+            
+            if (ex == 0)
+                return value1;
+            else if (ex < 0) {
+                BigDecimal one = BigDecimal.ONE;
+                BigDecimal value = value1.toBigDecimal().pow(ex * -1);
+                value = one.divide(value, scale, roundingMode.getBigDecimalRound());
+                
+                Num result = new Num(value);
+                return result;
+            } else {
+                BigDecimal value = value1.toBigDecimal().pow(exponent.toBigDecimal().intValue());
+                Num result = new Num(value);
+                return result;
+            }
+        }
+        
     }
 
     @Override
